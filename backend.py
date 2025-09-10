9e9192626054315e8b0d11655c058814bdae0c2c
quinncerino <quinncerino@gmail.com>
2025-09-10 12:58:48 -0400

    completed necessary imports

---
+++ backend.py
@@ -0,0 +1,5 @@
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.sentiment import SentimentIntensityAnalyzer
+
---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import plotly.express as px
+
+st.title("ToneTeller")
+st.header("I will analyze the tone of your text")
+
+st.text_input("Enter text here: ")
