806486e8f1abe4c664f05f13aae1eebab84f936d
quinncerino <quinncerino@gmail.com>
2025-09-10 13:10:52 -0400

    added emotion levels detection

--- backend.py
+++ backend.py
@@ -3,3 +3,30 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.sentiment import SentimentIntensityAnalyzer
 
+english_stopwords = stopwords.words("english")
+analyzer = SentimentIntensityAnalyzer()
+
+def get_levels(entry):
+    scores_list = []
+    pattern = re.compile(r'[A-Za-z]+')
+    words = re.findall(pattern, entry)
+
+    d = {}
+    for word in words:
+        if word in d.keys():
+            d[word] = d[word] + 1
+        else:
+            d[word] = 1
+    
+    d_list = [(value, key) for (key, value) in d.items()]
+    d_list.sort(reverse = True)
+
+    filtered_words = []
+    for count, word in d_list:
+        if word not in english_stopwords:
+            filtered_words.append((word, count))
+    
+    scores = analyzer.polarity_scores(entry)
+    scores_list.append(scores)
+    
+    return scores_list
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_levels
 
 st.title("ToneTeller")
 st.header("I will analyze the tone of your text")
 
-st.text_input("Enter text here: ")
+text = st.text_input("Enter text here: ")
+
+if text:
+    print(get_levels(text))
